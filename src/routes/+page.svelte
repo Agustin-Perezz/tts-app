2a6bb856e583a4e4ad5b951f121f5ba2e908c28b
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-07-10 09:05:54 -0300

    Change the background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import Background from '$lib/components/Background.svelte';
   import Tts from './Tts.svelte';
 </script>
 
-<div
-  class="flex min-h-screen flex-col items-center justify-center bg-gray-900 p-8 text-white"
->
-  <Tts />
+<div class="relative min-h-screen w-full bg-[#f8fafc]">
+  <Background />
+  <div
+    class="relative z-10 flex min-h-screen flex-col items-center justify-center p-8 text-white"
+  >
+    <Tts />
+  </div>
 </div>
