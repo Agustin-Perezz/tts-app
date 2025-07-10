619e67bac64552342d95d8c18585483afe19ce61
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-07-10 09:41:56 -0300

    Add fonts and modify the header

--- src/routes/TtsHeader.svelte
+++ src/routes/TtsHeader.svelte
@@ -1,4 +1,8 @@
-<div class="text-center">
-  <h1 class="mb-4 text-6xl font-bold text-white">Text to Speech</h1>
-  <p class="text-xl text-gray-300">Transform your text into natural speech</p>
+<div class="mt-28 text-center">
+  <h1 class="font-marker mb-3 text-5xl text-gray-700">
+    Minimal Text to Speech
+  </h1>
+  <p class="font-nothing text-2xl font-bold text-gray-600">
+    Powered by ElevenLabs
+  </p>
 </div>
